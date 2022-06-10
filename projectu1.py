258d7d0c0505088bbd4c3a3e267d05f13d377997
varsha1245 <93024268+varsha1245@users.noreply.github.com>
2022-06-10 10:10:33 +0530

    Update projectu1.py

--- projectu1.py
+++ projectu1.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import Scrollbar
 from bs4 import BeautifulSoup
-import requests
+
 import webbrowser
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
